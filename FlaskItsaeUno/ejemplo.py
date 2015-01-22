8cf0a46883f5dd9718cde65dfedfdeb59a1e4dc2
tortolin <hector7augusto@gmail.com>
2015-01-22 17:36:49 -0500

    Primer cambio en el proyecto de Python en Itsae

---
+++ FlaskItsaeUno/ejemplo.py
@@ -0,0 +1,8 @@
+'''
+Created on 22/1/2015
+
+@author: PC08
+'''
+
+if __name__ == '__main__':
+    pass
--- FlaskItsaeUno/run.py
+++ FlaskItsaeUno/run.py
@@ -2,6 +2,11 @@
 Created on 19/1/2015
 
 @author: PC08
+
+Augusto Noriega
+Itsae-Cade 
+2015
+Proyecto con github
 '''
 from app import app 
 
